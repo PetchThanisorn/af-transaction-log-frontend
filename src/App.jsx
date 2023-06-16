6fb3f422c749aa20eb68864337a249d41e8bdd56
Naay123 <naynaynayhell@gmail.com>
2023-06-16 16:45:54 +0700

    changeheader

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
         size={[0, 48]}
       >
         <Layout>
-          <Header style={headerStyle}>หัวเรื่อง</Header>
+          <Header style={headerStyle}>header</Header>
           <Content style={contentStyle }>Content</Content>
           <Footer style={footerStyle}>Footer</Footer>
         </Layout>
