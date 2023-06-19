0eae26eb496d5cd2eda543d020b3fb2a214a5cae
PetchThanisorn <Petchthanisorn2019@gmail.com>
2023-06-19 15:58:33 +0700

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Home from "./components/Home/Home";
 import { Layout, Space, Button } from "antd";
+
 const { Header, Footer, Sider, Content } = Layout;
 const headerStyle = {
   textAlign: "center",
@@ -11,29 +10,32 @@ const headerStyle = {
   height: 64,
   paddingInline: 50,
   lineHeight: "64px",
-  backgroundColor: "#7dbcea",
+  backgroundColor: "#b37feb",  
+  position:"sticky",
+  top:0
 };
 const contentStyle = {
   textAlign: "center",
   minHeight: `calc(100vh - 50px)`,
   lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#108ee9",
+  color: "#",
+  backgroundColor: "#d3adf7",
 };
 const siderStyle = {
   textAlign: "center",
   lineHeight: "120px",
   color: "#fff",
-  backgroundColor: "#3ba0e9",
+  backgroundColor: "#fff",
 };
 const footerStyle = {
   textAlign: "center",
   color: "#fff",
-  backgroundColor: "#7dbcea",
+  backgroundColor: "#b37feb",
   position:"sticky",
   bottom:0,
   minHeight: '50px'
 };
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -48,8 +50,8 @@ function App() {
         size={[0, 48]}
       >
         <Layout>
-          <Header style={headerStyle}>header</Header>
-          <Content style={contentStyle }>Content</Content>
+          <Header style={headerStyle}>Header</Header>
+          <Home style={contentStyle}></Home>
           <Footer style={footerStyle}>footer</Footer>
         </Layout>
       </Space>
