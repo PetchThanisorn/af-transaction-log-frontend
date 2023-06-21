97976c1cf36841cdc30beea1d0b015a8f8562e4e
PetchThanisorn <Petchthanisorn2019@gmail.com>
2023-06-21 11:10:38 +0700

    Added Array Number

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 import { UploadOutlined } from "@ant-design/icons";
 import { Button, message, Upload } from "antd";
+import { Space, Table, Tag } from 'antd';
+import { useCSVReader } from 'react-papaparse'
 
 // const [file, setFile] = useState("");
 
@@ -29,18 +31,77 @@ function readCSVFile(e) {
   }
 };
 
+
+const columns = [
+  {
+    title: 'Trans Date',
+    dataIndex: 'transferDate',
+    key: 'transferDate',
+  },
+  {
+    title: 'Effect Date',
+    dataIndex: 'effectDate',
+    key: 'effectDate',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'Debit',
+    dataIndex: 'debit',
+    key: 'debit',
+  },
+  {
+    title: 'Credit',
+    dataIndex: 'credit',
+    key: 'credit',
+  },
+  {
+    title: 'Balance',
+    dataIndex: 'balance',
+    key: 'balance',
+  },
+  {
+    title: 'Channel',
+    dataIndex: 'channel',
+    key: 'channel',
+  },
+];
+
+let numbers = [1, 2, 3, 4, 5];
+
+const data = [
+  {
+    transferDate: '1',
+    effectDate: 'John Brown',
+    description: '32',
+    debit: 'New York No. 1 Lake Park',
+    credit:'asd',
+    balance: 'asd',
+    channel: 'ds',
+  },  
+
+]; 
+
+
+
+
 const inputFileElement = (e)=>{
   console.log("Click");
   document.getElementById("upload-input").click();
 }
 
+
 function Home() {
   return (
     <div>
       <input id="upload-input" style={{display:"none"}} type="file" onChange={readCSVFile}></input>
-
       <Button icon={<UploadOutlined />} onClick={inputFileElement}>Click to Upload</Button>
-    
+
+      <box><Button onClick={""}>Remove</Button></box>
+      <Table columns={columns} dataSource={data}  />
     </div>
   );
 }
