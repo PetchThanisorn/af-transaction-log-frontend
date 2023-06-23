f6af2e57b8b5491042807d544e058271b9981657
Ratcharphol-Charlee <ratcharphol2020@gmail.com>
2023-06-23 14:47:50 +0700

    select data

--- src/components/SelectStatement/Select.jsx
+++ src/components/SelectStatement/Select.jsx
@@ -18,14 +18,10 @@ import "./Select.css";
 function SelectStatement() {
   const [month, setMonth] = useState([]);
   const [year, setYear] = useState([]);
-
   const [monthSelect, setMonthSelect] = useState("");
   const [yearSelect, setYearSelect] = useState("");
-
-  const yearMonth = {
-    2020: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    2019: ["1", "2", "3", "4", "5", "6", "7", "8"],
-  };
+  const [yearMonth, setyearMonth] = useState({});
+  const [list, setList] = useState([]);
   const monthStr = [
     "มกราคม",
     "กุมภาพันธ์",
@@ -42,19 +38,85 @@ function SelectStatement() {
   ];
 
   useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://127.0.0.1:3000/statement/period", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({}),
+      });
+      const result = await response.json();
+      setyearMonth(result['result']);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(()=>{
     let years = [];
     for (const [key, value] of Object.entries(yearMonth)) {
       console.log(value);
       years.push(key);
     }
     setYear(years);
-  }, []);
+    console.log(yearMonth);
+  },[yearMonth])
+
+  const selectApi = async (e) => {
+    let selected = yearSelect+ monthSelect;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ year: yearSelect, month: monthSelect,ascending:true }),
+    };
+    const response = await fetch(
+      "http://127.0.0.1:3000/statement/select",
+      requestOptions
+    );
+    const data = await response.json();
+    setList(data["result"])
+    console.log(data);
+  };
+  const columns = [
+    {
+      title: "Trans Date",
+      dataIndex: "transdate",
+      key: "transdate",
+    },
+    {
+      title: "Effect Date",
+      dataIndex: "effdate",
+      key: "effdate",
+    },
+    {
+      title: "Description",
+      dataIndex: "particular",
+      key: "particular",
+    },
+    {
+      title: "Debit",
+      dataIndex: "Withdrawal",
+      key: "Withdrawal",
+    },
+    {
+      title: "Credit",
+      dataIndex: "deposit",
+      key: "deposit",
+    },
+    {
+      title: "Balance",
+      dataIndex: "Balance",
+      key: "Balance",
+    },
+    {
+      title: "Channel",
+      dataIndex: "terminalno",
+      key: "terminalno",
+    },
+  ];
 
   return (
     <div className="Select">
-
-      <span>กรุณาเลือก ปี :</span>
-
+      <div>
+          <span>กรุณาเลือก ปี :</span>
       <Select
         style={{ width: 150, marginRight: 10 }}
         onChange={(value) => {
@@ -73,14 +135,23 @@ function SelectStatement() {
           setMonthSelect(value);
         }}
         options={month.map((month) => ({
-          label: monthStr[month-1] +" "+ yearSelect + "/" +month,
-          value: month.padStart(2, '0'),
+          label: monthStr[month - 1] + " " + yearSelect + "/" + month,
+          value: month.padStart(2, "0"),
         }))}
       />
 
-      <Button onClick={()=>{
-        console.log(yearSelect+monthSelect);
-      }}>ค้นหา</Button>
+      <Button
+        onClick={(e) => {
+          selectApi()
+        }}
+      >
+        ค้นหา
+      </Button>
+      </div>
+      <div>
+      {list.length == 0 ? "" : <Table dataSource={list} columns={columns} />}
+      </div>
+    
     </div>
   );
 }
